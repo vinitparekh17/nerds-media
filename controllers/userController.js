28596a1d97d166bea08584ba38afdf67bf97ddcc
Vinit Parekh <vinitrevaliya@gmail.com>
2022-07-08 21:08:24 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,12 +4,16 @@ const CLIENT_URL = "https://technetic.vercel.app"
 
 exports.loginSuccess = async (req, res, next) => {
     try {
+        if(req.user) {
             res.status(200).json({
                 success: true,
                 message: "login success",
                 user: req.user
             })
-        
+        } else {
+            res.status(404).json({
+                success: false
+            })
     } catch (error) {
         console.log(error);
     }
