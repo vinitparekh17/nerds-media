dacf28435121da5fe5fa8efcb8bdc2c940b54f2c
Vinit Parekh <vinitrevaliya@gmail.com>
2022-08-02 20:01:12 +0530

    file delete api

--- routes/study.js
+++ routes/study.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const { uploadFile, getFiles, getFilesBySubject } = require('../controllers/studyController')
+const { uploadFile, getFiles, getFilesBySubject, deleteFile } = require('../controllers/studyController')
 
 router.route('/uploadFile').post(uploadFile)
 router.route('/getFiles').get(getFiles)
 router.route('/getFilesBySubject').post(getFilesBySubject)
+router.route('/deletefile').post(deleteFile)
 
 module.exports = router;
